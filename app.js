5ad60acdfd2ddf8df1e00de886e2eaa0a1764ceb
shikow <shikowsp@gmail.com>
2018-03-11 21:19:32 -0300

    Added nconf sample :)

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nconf = require('nconf');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+nconf.file('config.js');
+nconf.defaults({
+    'http':{
+        'port': 3000
+    }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
